bf14156bc6c027704ee7754cd1415a28b02e6fcb
MANAF OFFICIAL <114959852+manafoffical007@users.noreply.github.com>
2024-06-29 18:12:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -25,7 +25,7 @@ async def start(client, message):
     await message.react(emoji=random.choice(REACTIONS))
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[            
-            InlineKeyboardButton('🎁 𝐈𝐍𝐓 𝐄𝐒𝐏𝐎𝐑𝐓𝐒 🎁', callback_data='help')
+            InlineKeyboardButton('🎁 𝐆𝐔𝐋𝐃 𝐈𝐍𝐅𝐎 🎁', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -45,7 +45,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🎁 𝐈𝐍𝐓 𝐄𝐒𝐏𝐎𝐑𝐓𝐒 🎁', callback_data='help')                       
+            InlineKeyboardButton('🎁 𝐆𝐔𝐋𝐃 𝐈𝐍𝐅𝐎 🎁', callback_data='help')                       
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
